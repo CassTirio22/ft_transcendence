fa939a54f64d1e9956a3446daf2cdcbd82ab382d
Anniball <lucas.delmas.an123@gmail.com>
2022-11-01 10:47:52 +0100

    fix: call to profile waits for token + onclick only to submit

--- client/src/screens/log/Register.tsx
+++ client/src/screens/log/Register.tsx
@@ -6,8 +6,8 @@ function Register() {
 	const [password, setPassword] = useState("");
 	const [userName, setUserName] = useState("");
 	const {user, register, profile} = useContext(AuthContext)
-	const handleSubmit = () => {
-		register(userMail, password, userName);
+	const handleSubmit = async () => {
+		await register(userMail, password, userName);
 		profile();
 	}
   return (
@@ -15,10 +15,12 @@ function Register() {
 		<div className='center_div'>
 			<h2>Register to create an account!</h2>
 		</div>
-		<div className="form" onClick={handleSubmit}>
+		<div className="form">
 			<input placeholder='Mail' type="text" value={userMail} onChange={(e) => setUserMail(e.target.value)} />
 			<input placeholder='Username' type="text" value={userName} onChange={(e) => setUserName(e.target.value)} />
 			<input placeholder='Password' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+		</div>
+		<div className="submition" onClick={handleSubmit}>
 			<button>Register</button>
 		</div>
 	</section>

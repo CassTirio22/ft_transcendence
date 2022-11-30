b8e2e469b212d88f802fc9e5e0a7eeca6ae910f8
Anniball <lucas.delmas.an123@gmail.com>
2022-11-30 16:33:53 +0100

    feat: entity, dto and service for game table

---
+++ server/src/api/game/game.entity.ts
@@ -0,0 +1,42 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum GameType {
+	friendly,
+	competitive
+}
+
+export enum GameStatus {
+	ongoing,
+	done,
+	interrupted
+}
+
+@Entity()
+export class Game extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	public id!: number;
+
+	@CreateDateColumn({ type: 'timestamp', default: () => "CURRENT_TIMESTAMP", name: "gameDate" })
+	public date!: Date;
+
+	@PrimaryColumn({ type: 'integer', name: "winnerId" })
+	public winner!: number;
+
+	@PrimaryColumn({ type: 'integer', name: "loserId" })
+	public loser!: number;
+
+	@Column({ type: 'integer', default: 0 })
+	public winnerScore!: number;
+	
+	@Column({ type: 'integer', default: 0 })
+	public loserScore!: number;
+	
+	@Column({ type: 'enum', enum: GameType, default: GameType.friendly})
+	public type!: GameType;
+
+	@Column({ type: 'enum', enum: GameStatus, default: GameStatus.ongoing})
+	public status!: GameStatus;
+
+	@Column({ type: 'integer', nullable: true })
+	public channel: number; 
+}

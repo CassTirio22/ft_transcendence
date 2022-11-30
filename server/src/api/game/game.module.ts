436aafc459e9391cff03f6d6ac978ca13a38ae1e
Anniball <lucas.delmas.an123@gmail.com>
2022-11-30 17:10:21 +0100

    feat: game module is done

--- server/src/api/game/game.module.ts
+++ server/src/api/game/game.module.ts
@@ -1,8 +1,14 @@
+import { UserModule } from '../user/user.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Game } from './game.entity'
+import { User } from '../user/user.entity'
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
+import { AuthModule } from '../user/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Game, User]), AuthModule, UserModule],
   controllers: [GameController],
   providers: [GameService]
 })

900b2ac4964059be4cdee7bb440579797ed1313b
Anniball <lucas.delmas.an123@gmail.com>
2022-12-01 16:27:39 +0100

    fix: friendhsip module is now running

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
+import { FriendshipModule } from './user/friendship/friendship.module';
 
 @Module({
-  imports: [UserModule, GameModule]
+  imports: [UserModule, GameModule, FriendshipModule]
 })
 export class ApiModule {}

c8dad8811d5478fc7ca668a0bc33194fc18125b0
ybrutout <brutout.yannah@gmail.com>
2022-12-06 12:46:53 +0100

    feat: Create the MessageModule

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
 import { FriendshipModule } from './user/friendship/friendship.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [UserModule, GameModule, FriendshipModule]
+  imports: [UserModule, GameModule, FriendshipModule, MessageModule]
 })
 export class ApiModule {}

48fa109e840dbb65dd475d05dca1ea5e7fc73ce7
Anniball <lucas.delmas.an123@gmail.com>
2022-12-15 16:48:06 +0100

    refactor: querybuilder and use of join in friendship service ongoing

--- server/src/api/user/friendship/friendship.service.ts
+++ server/src/api/user/friendship/friendship.service.ts
@@ -24,11 +24,6 @@ export class FriendshipService {
 			.leftJoinAndSelect("user.received", "received", "received.applicant = :applicantId", {applicantId: user.id})
 			.where("id = :friendId", {friendId: id})
 			.getOne();
-		console.log(this.userRepository.createQueryBuilder('user')
-		.select()
-		.leftJoinAndSelect("user.sent", "sent", "sent.solicited = :solicitedId", {solicitedId: id})
-		.leftJoinAndSelect("user.received", "received", "received.applicant = :applicantId", {applicantId: id})
-		.where("id = :friendId", {friendId: id}).getQuery());
 		if (!friend) {
 			throw new HttpException('Not found', HttpStatus.NOT_FOUND)
 		}
@@ -41,71 +36,63 @@ export class FriendshipService {
 			.execute()).generatedMaps[0] as Friendship;
 	}
 
-	public async responseFriend(body: ResponseFriendDto, req: Request): Promise<Friendship> {
+	public async responseFriend(body: ResponseFriendDto, req: Request): Promise<number> {
 		const { didAccept, applicant }: ResponseFriendDto = body;
 		const user: User = <User>req.user;
 
-		let friendship: Friendship = await this.friendshipRepository.createQueryBuilder()
-			.select()
-			.where("applicant_id = :friendId", {friendId: applicant})
-			.andWhere("solicited_id = :userId", {userId: user.id})
-			.getOne();
-		if (!friendship) {
-			throw new HttpException('Not found', HttpStatus.NOT_FOUND)
-		}
-		else if (friendship.status != FriendshipStatus.pending) {
-			throw new HttpException('Conflict', HttpStatus.CONFLICT)
-		}
-		//probably can be done without the select at first, will return empty JSON if nothing is updated
 		return (await this.friendshipRepository.createQueryBuilder()
 			.update()
 			.set( { status: didAccept ? FriendshipStatus.accepted : FriendshipStatus.rejected } )
-			.where( "solicited_id = :userId", {userId: user.id} )
-			.andWhere( "applicant_id = :friendId", {friendId: applicant})
-			.execute()).generatedMaps[0] as Friendship;
+			.where("solicited_id = :userId", {userId: user.id} )
+			.andWhere("applicant_id = :friendId", {friendId: applicant})
+			.andWhere("friendship.status = :pendingStatus", {pendingStatus: FriendshipStatus.pending})
+			.execute()).affected;
 	}
 
 	// SELECT * FROM User WHERE Id IN CONCAT( ( SELECT applicant FROM Friendship WHERE solicited = user.id), ( SELECT solicited FROM Friendship WHERE applicant = user.id) )
 	public async friends(user: User): Promise< User[] | never > {
+		return (await this.userRepository.createQueryBuilder('user')
+			.select()
+			.innerJoin("user.received", "rec", "rec.status = :recStatus AND rec.applicant = :appId", {recStatus: FriendshipStatus.accepted, appId: user.id})
+			.innerJoin("user.sent", "sent", "sent.status = :sentStatus AND rec.solicited = :solId", {sentStatus: FriendshipStatus.accepted, solId: user.id})
+			.getMany());
+
 		//must be done using querybuilder and probably inner joins
-		return (await this.userRepository.find ({
-			where: {
-				id: In(
-					(await this.friendshipRepository.find({
-						select: ["applicant"],
-						where: {
-							"solicited_id": user.id,
-							"status": FriendshipStatus.accepted,
-						}
-					})).map(Friendship => Friendship.applicant)
+		// return (await this.userRepository.find ({
+		// 	where: {
+		// 		id: In(
+		// 			(await this.friendshipRepository.find({
+		// 				select: ["applicant"],
+		// 				where: {
+		// 					"solicited_id": user.id,
+		// 					"status": FriendshipStatus.accepted,
+		// 				}
+		// 			})).map(Friendship => Friendship.applicant)
 					
-					.concat(
-					(await this.friendshipRepository.find({
-						select: ["solicited"],
-						where: {
-							"applicant_id": user.id,
-							"status": FriendshipStatus.accepted,
-						}
-					})).map(Friendship => Friendship.solicited))
-				),
-			}
-		}));
+		// 			.concat(
+		// 			(await this.friendshipRepository.find({
+		// 				select: ["solicited"],
+		// 				where: {
+		// 					"applicant_id": user.id,
+		// 					"status": FriendshipStatus.accepted,
+		// 				}
+		// 			})).map(Friendship => Friendship.solicited))
+		// 		),
+		// 	}
+		// }));
 	}
 
 	public async deleteFriend(body: DeleteFriendDto, req: Request): Promise<number> {
 		const { friend } : DeleteFriendDto = body;
 		const user: User = <User>req.user;
 
+		if (user.id == friend) {
+			throw new HttpException('Conflict', HttpStatus.CONFLICT);
+		}
 		return (await this.friendshipRepository.createQueryBuilder()
 			.delete()
-			.where( new Brackets (query => { query
-				.where("applicant_id = :applicantFriendId", {applicantFriendId: friend})
-				.andWhere("solicited_id = :solicitedUserId", {solicitedUserId: user.id})
-			}))
-			.orWhere( new Brackets (query => { query
-				.where("solicited_id = :solicitedFriendId", {solicitedFriendId: friend})
-				.andWhere("applicant_id = :applicantUserId", {applicantUserId: user.id})
-			}))
+			.where("applicant_id IN (:...friendsId_1)", {friendsId_1: [user.id, friend]})
+			.andWhere("solicited_id IN (:...friendsId_2)", {friendsId_2: [user.id, friend]})
 			.execute()).affected;
 	}
 }

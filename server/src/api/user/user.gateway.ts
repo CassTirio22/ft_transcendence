d46243efd086235410056f5ed90a9b8f34df6d75
Anniball <lucas.delmas.an123@gmail.com>
2023-01-26 16:47:03 +0100

    chore: some print and commented block service on sockets

--- server/src/api/user/user.gateway.ts
+++ server/src/api/user/user.gateway.ts
@@ -59,21 +59,22 @@ export class UserGatewayUtil {
 		let channelId: string = this.defineChannelId(message);
 		let member: Member = await this.memberService.memberBySocket(client.id);
 		let user: User = await this.userService.userBySocket(client.id);
-		if (!client.rooms.has(channelId) || member.status == MemberStatus.muted) {
+		if (!client.rooms.has(channelId) || (member != null && member.status == MemberStatus.muted)) {
 			return false;
 		}
 		//DON'T ADD IN DB ON GATEWAY-SIDE FOR NOW
 		// if (message.channel_id)
 		// 	await this.messageService.sendChannel({origin: message.channel_id, content: message.content}, user);
 		
-		const blockers: User[] = await this.blockService.getBlockerList(user);
-		const blockerSockets: Socket[] = clients;
-		blockerSockets.filter( elem => blockers.find( blocker => blocker.socket == elem.id ) != undefined );
-		blockerSockets.forEach( blocker => blocker.join('blockRoom') )
+		// const blockers: User[] = await this.blockService.getBlockerList(user);
+		// const blockerSockets: Socket[] = clients;
+		// blockerSockets.filter( elem => blockers.find( blocker => blocker.socket == elem.id ) != undefined );
+		// blockerSockets.forEach( blocker => blocker.join('blockRoom') )
 
-		client.to(channelId).except('blockRoom').emit('message', message);
+		console.log("shoudl send message to sockets: " + clients.filter( client => client.rooms.has(channelId) ));
+		client.to(channelId).emit('messages', message);
 
-		blockerSockets.forEach( blocker => blocker.leave('blockRoom'));
+		// blockerSockets.forEach( blocker => blocker.leave('blockRoom'));
 
 		return true;
 	}
@@ -228,6 +229,7 @@ export class UserGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 	//client send message to channel
 	@SubscribeMessage("message")
 	handleMessage(client: Socket, data: DiscussionMessage) {
+		console.log("received message : " + data);
 		if (!this.util.emitMessage(client, data, this.clients))
 			client.emit('error', {message: 'Sending messages in this channel unauthorized.'});
 	}

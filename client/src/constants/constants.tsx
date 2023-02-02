cc414f99977d3c495adb6997bc98d097dcccc90d
Anniball <lucas.delmas.an123@gmail.com>
2023-02-02 11:53:58 +0100

    feat: ongoing compatibility docker

--- client/src/constants/constants.tsx
+++ client/src/constants/constants.tsx
@@ -24,8 +24,8 @@ export const CHANNEL_LVL = {
     PRIVATE: 2
 }
 
-export const base_url = "http://localhost:5000";
-export const socket_url = "ws://localhost:5000";
+export const base_url = "http://10.1.10.1:5000";
+export const socket_url = "ws://10.1.10.1:5000";
 export const intra_url = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-1eaad37c69601826513dcbd2aad3181a977d8eeedfa631117021f93c40e84db0&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Foauth&response_type=code&scope=public";
 
 type User = {
